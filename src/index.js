34da7a7d30e526fe21d3a2cf28d2304b08497905
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-02-19 15:48:49 +0500

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-module.exports = function toReadable (number) {
-  
-}
+const numbers = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+};
+
+module.exports = function toReadable(number) {
+  const numStr = number.toString();
+  const noHundred = number % 100;
+  if (numStr.length < 3) {
+    return number < 20 || !(number % 10) ? numbers[number] : numbers[`${numStr[0]}0`] + ' ' + numbers[numStr[1]];
+  } else {
+    return !noHundred
+      ? `${numbers[numStr[0]]} hundred`
+      : !(number % 10) || noHundred < 20
+      ? `${numbers[numStr[0]]} hundred ${numbers[noHundred]}`
+      : `${numbers[numStr[0]]} hundred ${numbers[`${numStr[1]}0`]} ${numbers[numStr[2]]}`;
+  }
+};
